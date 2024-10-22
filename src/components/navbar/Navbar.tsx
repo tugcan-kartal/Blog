b62e1df3f4bc1e9501a051daa2518633f939b605
tugcan-kartal <tugcankartal01@gmail.com>
2024-10-22 17:13:46 +0300

    first commit

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar:React.FC=()=>{
+
+    const data=[
+        {
+            title: "Home",
+            url: "/"
+        },
+        {
+            title: "Portfolio",
+            url: "/portfolio"
+        },
+        {
+            title: "Blog",
+            url: "/blog"
+        },
+        {
+            title: "About",
+            url: "/about"
+        },
+        {
+            title: "Contact",
+            url: "/contact"
+        },
+        {
+            title: "Dashboard",
+            url: "/dashboard"
+        }
+    ]
+
+    return(
+        <div>
+            <div className="flex justify-between mt-[5vh]">
+
+                <div>
+                    <div>Tugcan</div>
+                </div>
+
+                <div className="flex gap-x-[2vw]">
+                    {data.map((item,index)=>(
+                        <div key={index}>
+                            <Link href={item.url}>{item.title}</Link>
+                        </div>
+                    ))}
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
