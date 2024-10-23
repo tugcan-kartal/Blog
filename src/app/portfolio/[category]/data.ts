81ce53a8ed01204b4e3ed70e0fa69b0c577ff124
tugcan-kartal <tugcankartal01@gmail.com>
2024-10-23 21:58:35 +0300

    bug fixed kategory

---
+++ src/app/portfolio/[category]/data.ts
@@ -0,0 +1,38 @@
+export const products={
+    websites: [
+        {
+            id: 1,
+            title: "Newai",
+            desc: "Interactive news site with artificial intelligence called newai.",
+            image: "https://tugcankartal.netlify.app/static/media/newai.31b3a773037755036001.jpeg"
+        },
+        {
+            id: 2,
+            title: "EasyOrder",
+            desc: "Ultimate restaurant POS software that helps serve your valued customers in a practical, productive manner and clone of sambapos.",
+            image: "https://tugcankartal.netlify.app/static/media/easyorderr.a7b473c7f4dda1a917f9.png"
+        },
+        {
+            id: 3,
+            title: "Coin",
+            desc: "Coin App is a crypto app that allows users to search for information about various cryptocurrencies in real-time.",
+            image: "https://tugcankartal.netlify.app/static/media/coinapp.bb2ed78a683eab6b1dc1.png"
+        },
+
+    ],
+    applications: [
+        {
+            id: 1,
+            title: "Car Rent",
+            desc: "A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for searching, comparing, and reserving cars.",
+            image: "https://tugcankartal.netlify.app/static/media/carrent.0b006fe2ae2afd17bb1e.png"
+        },
+        {
+            id: 2,
+            title: "Kitchen Shop",
+            desc: "A kitchen website is a comprehensive resource for kitchen tools information options, and tools to help users achieve their meal choice.",
+            image: "https://tugcankartal.netlify.app/static/media/kitchenapp.7a346135980fba920ba5.png"
+        },
+    ]
+    
+}
--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,21 +1,47 @@
 "use client"
 
-import { useParams } from 'next/navigation'; // Correct import
-import React from 'react';
+import { notFound, useParams } from "next/navigation";
+import React from "react";
+import { products } from "./data";
 
-const Category: React.FC = () => {
-    const params = useParams(); // Access the params
-    const categoryName = params.category as string; // Access the category parameter
 
-    if (!categoryName) {
-        return <div>Loading...</div>; // Fallback for when the param isn't ready yet
+const Category:React.FC=()=>{
+
+    const params=useParams();
+    const categoryName=params.category as "websites" | "applications";
+
+    const getDatas=(catName: "websites" | "applications")=>{
+        const allDatas=products[catName];
+        if(allDatas){
+            return allDatas
+        }
+
+        return notFound();
     }
 
-    return (
-        <div className="mt-10">
-            <h1 className="text-2xl font-bold">{categoryName.toUpperCase()} Gallery</h1>
+    const datas=getDatas(categoryName);
+
+    return(
+        <div>
+            {datas && datas.map((item,index)=>(
+                <div key={index}>
+
+                    <div className="flex justify-center gap-x-[10vw]">
+                        <div className="w-[25vw]">
+                            <div>{item.title}</div>
+                            <div>{item.desc}</div>
+                        </div>
+
+                        
+                        <div className="w-[25vw]">
+                            <img src={item.image} alt="not found"/>
+                        </div>
+                    </div>
+
+                </div>
+            ))}
         </div>
-    );
-};
+    )
+}
 
 export default Category;
