57a18cfededd9583d5006ed87e15836e53ff013a
tugcan-kartal <tugcankartal01@gmail.com>
2024-11-02 17:32:09 +0300

    added slick

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -4,6 +4,9 @@ import { notFound, useParams } from "next/navigation";
 import React from "react";
 import { products } from "./data";
 import Link from "next/link";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Category: React.FC = () => {
     const params = useParams();
@@ -20,12 +23,21 @@ const Category: React.FC = () => {
 
     const datas = getDatas(categoryName);
 
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      };
+
+
     return (
         <div className="container mx-auto py-12 px-4">
             <h1 className="text-4xl font-bold text-center mb-10 text-gray-800">{categoryName.charAt(0).toUpperCase() + categoryName.slice(1)}</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <Slider {...settings} className="lg:w-[25vw] mx-auto">
                 {datas && datas.map((item, index) => (
-                    <div key={index} className="bg-white rounded-lg shadow-xl overflow-hidden transition-transform transform hover:scale-105 duration-300">
+                    <div  key={index} className="bg-white rounded-lg shadow-xl overflow-hidden transition-transform transform hover:scale-105 duration-300">
                         <div className="relative">
                             <img src={item.image} alt={item.title} className="w-full h-64 object-cover rounded-t-lg" />
                             <div className="absolute top-0 left-0 p-4 bg-blue-600 text-white rounded-br-lg">
@@ -40,7 +52,8 @@ const Category: React.FC = () => {
                         </div>
                     </div>
                 ))}
-            </div>
+            </Slider>
+           
         </div>
     );
 }
