432b8d9a0fabd807db6c3a2e10ffd2222c0c33f5
tugcan-kartal <tugcankartal01@gmail.com>
2024-11-02 17:49:10 +0300

    updated links image

--- src/app/portfolio/[category]/data.ts
+++ src/app/portfolio/[category]/data.ts
@@ -4,28 +4,28 @@ export const products={
             id: 1,
             title: "Newai",
             desc: "Interactive news site with artificial intelligence called newai.",
-            image: "https://tugcankartal.netlify.app/static/media/newai.31b3a773037755036001.jpeg",
+            image: "https://r.resimlink.com/2VUzJM.jpeg",
             url: "https://66a113b6c0cd8e423184f7e7--classy-donut-34f99c.netlify.app/"
         },
         {
             id: 2,
             title: "EasyOrder",
             desc: "Ultimate restaurant POS software that helps serve your valued customers in a practical, productive manner and clone of sambapos.",
-            image: "https://tugcankartal.netlify.app/static/media/easyorderr.a7b473c7f4dda1a917f9.png",
+            image: "https://r.resimlink.com/1QGNphv.png",
             url: "https://6584152ccaaaa7d355ca9300--easyorderr.netlify.app/"
         },
         {
             id: 3,
             title: "Coin",
             desc: "Coin App is a crypto app that allows users to search for information about various cryptocurrencies in real-time.",
-            image: "https://tugcankartal.netlify.app/static/media/coinapp.bb2ed78a683eab6b1dc1.png",
+            image: "https://r.resimlink.com/djVQyeUNb16.png",
             url: "https://641b222dc64c0d18a38466d5--coinappbytugcankartal.netlify.app/"
         },
         {
             id: 4,
             title: "Citio",
             desc: "İstanbul Hotel website",
-            image: "https://tugcankartal.netlify.app/static/media/coinapp.bb2ed78a683eab6b1dc1.png",
+            image: "https://r.resimlink.com/kiPExoq.png",
             url: "https://citioapp.com"
         },
 
