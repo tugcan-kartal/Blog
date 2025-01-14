a0ba7937e03143052d906a447484ead2f1f0eca7
tugcan-kartal <tugcankartal01@gmail.com>
2025-01-14 17:37:05 +0300

    cv changed

--- src/app/portfolio/[category]/data.ts
+++ src/app/portfolio/[category]/data.ts
@@ -9,24 +9,24 @@ export const products={
         },
         {
             id: 2,
+            title: "Citio",
+            desc: "A site that allows you to easily arrange events, tours, and hotels in Istanbul. Users can browse options to find services that suit their needs.",
+            image: "https://r.resimlink.com/kiPExoq.png",
+            url: "https://citioapp.com"
+        },
+        {
+            id: 3,
             title: "EasyOrder",
             desc: "Ultimate restaurant POS software that helps serve your valued customers in a practical, productive manner and clone of sambapos.",
             image: "https://r.resimlink.com/1QGNphv.png",
             url: "https://6584152ccaaaa7d355ca9300--easyorderr.netlify.app/"
         },
         {
-            id: 3,
+            id: 4,
             title: "Coin",
             desc: "Coin App is a crypto app that allows users to search for information about various cryptocurrencies in real-time.",
             image: "https://r.resimlink.com/djVQyeUNb16.png",
             url: "https://641b222dc64c0d18a38466d5--coinappbytugcankartal.netlify.app/"
-        },
-        {
-            id: 4,
-            title: "Citio",
-            desc: "A site that allows you to easily arrange events, tours, and hotels in Istanbul. Users can browse options to find services that suit their needs.",
-            image: "https://r.resimlink.com/kiPExoq.png",
-            url: "https://citioapp.com"
         },
 
     ],
