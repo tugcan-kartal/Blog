459158252b9eeab4f25dae6e700437b357e0715c
tugcan-kartal <tugcankartal01@gmail.com>
2025-04-02 08:35:20 +0300

    updated

--- src/app/portfolio/[category]/data.ts
+++ src/app/portfolio/[category]/data.ts
@@ -1,46 +1,47 @@
 export const products={
     websites: [
+        
         {
             id: 1,
-            title: "Newai",
-            desc: "Interactive news site with artificial intelligence called newai.You can ask news-related questions to artificial intelligence",
-            image: "https://r.resimlink.com/2VUzJM.jpeg",
-            url: "https://newai-tau.vercel.app/"
-        },
-        {
-            id: 2,
             title: "Citio",
             desc: "A site that allows you to easily arrange events, tours, and hotels in Istanbul. Users can browse options to find services that suit their needs.",
             image: "https://r.resimlink.com/kiPExoq.png",
             url: "https://citioapp.com"
         },
         {
-            id: 3,
-            title: "EasyOrder",
-            desc: "Ultimate restaurant POS software that helps serve your valued customers in a practical, productive manner and clone of sambapos.",
-            image: "https://r.resimlink.com/1QGNphv.png",
-            url: "https://new-easy-order.vercel.app/"
-        },
-        {
-            id: 4,
-            title: "Coin",
-            desc: "Coin App is a crypto app that allows users to search for information about various cryptocurrencies in real-time.",
-            image: "https://r.resimlink.com/djVQyeUNb16.png",
-            url: "https://coin-app-seven.vercel.app/"
-        },
-        {
-            id: 5,
+            id: 2,
             title: "Taskly Adviser AI",
             desc: "With the free AI-powered task manager, everything is in place: your work life is organized, your private life is comfortable.",
             image: "https://r.resimlink.com/S3_Ul.jpg",
             url: "https://www.tasklyadviserai.com/"
         },
         {
-            id: 6,
+            id: 3,
             title: "Taskly Adviser AI Introduction",
             desc: "Taskly Adviser AI Introduction Website",
             image: "https://r.resimlink.com/ADJSHW.jpg",
             url: "https://infotasklyadviseraicom.vercel.app/"
+        },
+        {
+            id: 4,
+            title: "Newai",
+            desc: "Interactive news site with artificial intelligence called newai.You can ask news-related questions to artificial intelligence",
+            image: "https://r.resimlink.com/2VUzJM.jpeg",
+            url: "https://newai-tau.vercel.app/"
+        },
+        {
+            id: 5,
+            title: "Coin",
+            desc: "Coin App is a crypto app that allows users to search for information about various cryptocurrencies in real-time.",
+            image: "https://r.resimlink.com/djVQyeUNb16.png",
+            url: "https://coin-app-seven.vercel.app/"
+        },
+        {
+            id: 6,
+            title: "EasyOrder",
+            desc: "Ultimate restaurant POS software that helps serve your valued customers in a practical, productive manner and clone of sambapos.",
+            image: "https://r.resimlink.com/1QGNphv.png",
+            url: "https://new-easy-order.vercel.app/"
         },
 
     ],
