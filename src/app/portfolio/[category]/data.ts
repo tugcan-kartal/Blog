ef4e2f9c6ccad689e63da8bbc725364144bc8fc8
tugcan-kartal <tugcankartal01@gmail.com>
2025-04-02 08:33:05 +0300

    updated

--- src/app/portfolio/[category]/data.ts
+++ src/app/portfolio/[category]/data.ts
@@ -5,7 +5,7 @@ export const products={
             title: "Newai",
             desc: "Interactive news site with artificial intelligence called newai.You can ask news-related questions to artificial intelligence",
             image: "https://r.resimlink.com/2VUzJM.jpeg",
-            url: "https://66a113b6c0cd8e423184f7e7--classy-donut-34f99c.netlify.app/"
+            url: "https://newai-tau.vercel.app/"
         },
         {
             id: 2,
@@ -19,14 +19,28 @@ export const products={
             title: "EasyOrder",
             desc: "Ultimate restaurant POS software that helps serve your valued customers in a practical, productive manner and clone of sambapos.",
             image: "https://r.resimlink.com/1QGNphv.png",
-            url: "https://6584152ccaaaa7d355ca9300--easyorderr.netlify.app/"
+            url: "https://new-easy-order.vercel.app/"
         },
         {
             id: 4,
             title: "Coin",
             desc: "Coin App is a crypto app that allows users to search for information about various cryptocurrencies in real-time.",
             image: "https://r.resimlink.com/djVQyeUNb16.png",
-            url: "https://641b222dc64c0d18a38466d5--coinappbytugcankartal.netlify.app/"
+            url: "https://coin-app-seven.vercel.app/"
+        },
+        {
+            id: 5,
+            title: "Taskly Adviser AI",
+            desc: "With the free AI-powered task manager, everything is in place: your work life is organized, your private life is comfortable.",
+            image: "https://r.resimlink.com/S3_Ul.jpg",
+            url: "https://www.tasklyadviserai.com/"
+        },
+        {
+            id: 6,
+            title: "Taskly Adviser AI Introduction",
+            desc: "Taskly Adviser AI Introduction Website",
+            image: "https://r.resimlink.com/ADJSHW.jpg",
+            url: "https://infotasklyadviseraicom.vercel.app/"
         },
 
     ],
